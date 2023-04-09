d0f87022b2a76170f72fb3223e10fc4bf7bfd831
Ishan Zuaim <ishanzuaim@gmail.com>
2023-04-09 11:22:13 +1000

    Added config.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Archivo } from "next/font/google";
 
+const archivo = Archivo({
+  weight: "300",
+  subsets: ["latin"],
+});
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={archivo.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
